a7df1ec94e8a19ff19a1887914026907db408b56
Alsrory <wwwalsarory55@gmail.comm>
2024-11-22 23:41:51 +0300

    r

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   
-        output: 'export', // This allows you to export your app as a static site
-        // Add other configurations as necessary
-    
       
       
 };
